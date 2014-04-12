d09d4f9e4f7e85a283dee6d1dac59aaa2fbccf80
arendu <adithya.r@gmail.com>
2014-04-12 00:05:20 -0400

    found a bug in the inflexion point merge code

--- line_segments.py
+++ line_segments.py
@@ -22,23 +22,19 @@ if __name__ == '__main__':
     optparser = optparse.OptionParser()
     optparser.add_option("-k", "--kbest-list", dest="input", default="data/train.100best", help="100-best translation lists")
     optparser.add_option("-r", "--reference", dest="reference", default="data/train.ref", help="Target language reference sentences")
-    optparser.add_option("-l", "--lm", dest="lm", default=0.23759745834155052, type="float", help="Language model weight")
+    optparser.add_option("-l", "--lm", dest="lm", default=-1.0, type="float", help="Language model weight")
     optparser.add_option("-t", "--tm1", dest="tm1", default=-0.5, type="float", help="Translation model p(e|f) weight")
     optparser.add_option("-s", "--tm2", dest="tm2", default=-0.5, type="float", help="Lexical translation model p_lex(f|e) weight")
     (opts, _) = optparser.parse_args()
     weights = {'p(e)': float(opts.lm), 'p(e|f)': float(opts.tm1), 'p_lex(f|e)': float(opts.tm2)}
     all_hyps = [pair.split(' ||| ') for pair in open(opts.input)]
     all_refs = [ref.strip() for ref in open(opts.reference)]
-    inflexion_points = []
-    #find lines using first feature 'p(e)'
     num_sents = len(all_hyps) / 100
 
     for w in weights:
         print 'current weights', weights
-        if w == 'p(e)':
-            continue
-
-        for s in xrange(0, 50):
+        inflexion_points = []
+        for s in xrange(0, 100):
             ref = all_refs[s]
             hyps_for_one_sent = all_hyps[s * 100:s * 100 + 100]
             lines = []
@@ -55,24 +51,26 @@ if __name__ == '__main__':
                         m += weights[k] * float(v)
                 lines.append((m, c, hyp, ref))
             sorted_lines = sorted(lines)
-            #pprint(sorted_lines)
             ip = sl.get_ranges(sl.get_upper_intersections(sorted_lines))
             inflexion_points += ip
-        #pprint(inflexion_points)
-        inflexion_points = sorted(inflexion_points)
-        range_markers_dict = dict(((inflexion_points[i][0], inflexion_points[i + 1][0]), []) for i in xrange(len(inflexion_points) - 1) if (
-            inflexion_points[i][0] != inflexion_points[i + 1][0]))
-        range_markers_dict[(inflexion_points[-1][0], inflexion_points[-1][1])] = []
+
+        inflexion_points.sort()
+        pprint(inflexion_points)
+        print '\n'
+        ranges = [(x1, x2) for x1, x2, hyp, ref in inflexion_points]
+        range_markers_key = list(set(sl.chain(ranges)))
+        range_markers_key.sort()
+        print '\nrange markers key\n', range_markers_key
+        range_markers_dict = dict(((range_markers_key[i], range_markers_key[i + 1]), []) for i in xrange(len(range_markers_key) - 1))
+        #range_markers_dict[(inflexion_points[-1][0], inflexion_points[-1][1])] = []
+        print '\nrange markers_dict\n',
+        pprint(range_markers_dict)
 
         for (x1, x2, h, r) in inflexion_points:
             for mx1, mx2 in sorted(range_markers_dict):
-
                 if x1 <= mx1 and mx2 <= x2:
-                    #print x1, x2, 'vs', mx1, mx2, 'inside'
                     range_markers_dict[mx1, mx2].append((h, r))
-                else:
-                    #print x1, x2, 'vs', mx1, mx2
-                    pass
+
         print 'line segments...'
         #pprint(range_markers_dict)
         print 'bleu score for ranges...'
--- sweep_line.py
+++ sweep_line.py
@@ -1,4 +1,11 @@
 __author__ = 'arenduchintala'
+from pprint import pprint
+
+
+def chain(points):
+    for p in points:
+        yield p[0]
+        yield p[1]
 
 
 def get_upper_intersections(sort_seg):
@@ -14,7 +21,7 @@ def get_upper_intersections(sort_seg):
             if intersect[0] is not None and intersect[1] is not None:
                 points_to_lines[intersect] = j
             else:
-                print 'skipping intersection...', a, b
+                print 'skipping intersection...', a, b, intersect
                 pass
         (mx, my, mhyp_pair, ref) = min(points_to_lines)
         upper.append((mx, my, mhyp_pair, ref))
@@ -43,8 +50,7 @@ def intersect_point(a1, a2):
     try:
         x = (a1[1] - a2[1]) / (a2[0] - a1[0])
     except:
-        print 'returning none...'
-        print a1, a2
+        print 'returning none...', a1, a2
         return None, None, (None, None), None
     y = a1[0] * x + a1[1]
     #return None, None, (None, None), None
@@ -52,8 +58,31 @@ def intersect_point(a1, a2):
 
 
 if __name__ == '__main__':
-    seg = [[-0.5, 2, 'A', 'ref'], [-0.5, -1, 'B', 'ref'], [0.2, 1, 'C', 'ref'], [5, -40, 'D', 'ref']]
-    sorted_seg = sorted(seg)
-    print get_upper_intersections(sorted_seg)
-    print get_ranges(get_upper_intersections(sorted_seg))
+    seg = [[-0.5, 2, 'A1', 'ref'], [-0.95, -1, 'B1', 'ref'], [0.2, 1, 'C1', 'ref'], [5, -40, 'D1', 'ref']]
+    seg2 = [[-0.15, 2, 'A2', 'ref2'], [-1.95, -1, 'B2', 'ref2'], [1.2, 1, 'C2', 'ref2'], [0.5, -40, 'D2', 'ref2']]
+    segs = [seg, seg2]
 
+    inflexion_points = []
+    for s in segs:
+        sorted_seg = sorted(s)
+        inflexion_points += get_ranges(get_upper_intersections(sorted_seg))
+
+    inflexion_points.sort()
+    pprint(inflexion_points)
+    print '\n'
+    ranges = [(x1, x2) for x1, x2, hyp, ref in inflexion_points]
+    range_markers_key = list(set(chain(ranges)))
+    range_markers_key.sort()
+    print '\nrange markers key\n', range_markers_key
+    range_markers_dict = dict(((range_markers_key[i], range_markers_key[i + 1]), []) for i in xrange(len(range_markers_key) - 1))
+    #range_markers_dict[(inflexion_points[-1][0], inflexion_points[-1][1])] = []
+    print '\nrange markers_dict\n',
+    pprint(range_markers_dict)
+
+    for (x1, x2, h, r) in inflexion_points:
+        for mx1, mx2 in sorted(range_markers_dict):
+            if x1 <= mx1 and mx2 <= x2:
+                range_markers_dict[mx1, mx2].append((h, r))
+
+    print '\nrange markers filled\n',
+    pprint(range_markers_dict)
